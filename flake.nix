dfd398df8a7defb5e4e03a017f5e3bd3ab698188
jadc <chehimj@gmail.com>
2025-07-18 19:05:37 -0400

    tweak: more complete nix flake

--- flake.nix
+++ flake.nix
@@ -20,13 +20,26 @@
                         cudaPackages.cuda_cudart
                         cudaPackages.cuda_nvcc
                         cudaPackages.cudatoolkit
+                        gcc13
                     ];
 
                     shellHook = ''
-                        export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
-                        export PATH=$CUDA_PATH/bin:$PATH
-                        export LIBRARY_PATH=$CUDA_PATH/lib:$LIBRARY_PATH
-                        export LD_LIBRARY_PATH=$CUDA_PATH/lib:/run/opengl-driver/lib:/run/opengl-driver-32
+                        export CUDA_PATH=${pkgs.cudatoolkit}
+                        export CC=${pkgs.gcc13}/bin/gcc
+                        export CXX=${pkgs.gcc13}/bin/g++
+                        export PATH=${pkgs.gcc13}/bin:$PATH
+                        export LD_LIBRARY_PATH=${
+                            pkgs.lib.makeLibraryPath [
+                                "/run/opengl-driver"
+                                pkgs.cudaPackages.cudatoolkit
+                                pkgs.cudaPackages.cudnn
+                            ]
+                        }:$LD_LIBRARY_PATH
+                        export LIBRARY_PATH=${
+                            pkgs.lib.makeLibraryPath [
+                                pkgs.cudaPackages.cudatoolkit
+                            ]
+                        }:$LIBRARY_PATH
                     '';
                 };
             });
